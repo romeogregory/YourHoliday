76ba4a22937e626a3fdf34b865fac88390cb5720
Romeo Lie-A-Tjam <romeogregoryx@gmail.com>
2022-05-11 14:45:57 +0200

    Quick changes

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppNavigator } from './components/navigation.component';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <AppNavigator/>
+      </SafeAreaView>
+    </ApplicationProvider>
+  </>
+);
---
+++ components/navigation.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from './home.component';
+import { DetailsScreen } from './details.component';
+import { BottomNavigation, BottomNavigationTab, Layout, Text, Icon  } from '@ui-kitten/components';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const CalendarIcon = (props) => (
+    <Icon {...props} name='calendar-outline'/>
+);
+
+const ClockIcon = (props) => (
+    <Icon {...props} name='clock-outline'/>
+);
+
+const SettingsIcon = (props) => (
+    <Icon {...props} name='settings-outline'/>
+);
+
+const HeartIcon = (props) => (
+    <Icon {...props} name='heart-outline'/>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab icon={CalendarIcon} />
+    <BottomNavigationTab icon={ClockIcon} />
+    <BottomNavigationTab icon={HeartIcon} />
+    <BottomNavigationTab icon={SettingsIcon} />
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name='Upcoming Holidays' icon={CalendarIcon} component={HomeScreen}/>
+    <Screen name='Until Next Holiday'  icon={ClockIcon} component={DetailsScreen}/>
+    <Screen name='About Us'  icon={HeartIcon} component={DetailsScreen}/>
+    <Screen name='Settings'  icon={SettingsIcon} component={DetailsScreen}/>
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <TabNavigator/>
+  </NavigationContainer>
+);
